4c6632fc9ebd5cb48efd9f1510a88f64343e5ea6
“Bizcom56” <dornorjoshua56@gmail.com>
2024-09-03 02:08:50 -0700

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,23 +5,24 @@ const request = require('request');
 const filmNum = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-// An API request to get film information
+// Makes an API request to get film information
 request(filmURL + filmNum, async function (err, res, body) {
   if (err) return console.error(err);
 
-  // A parse response body which gets the list of character URLs
+  // Parse the response body to get the list of character URLs
   const charURLList = JSON.parse(body).characters;
 
-  // iterating through a list of character URLs and making a request to each URL to retrieve the corresponding character information.
+  // Iterare through the character URLs and fect character information
+  // Make a request to each character URL
   for (const charURL of charURLList) {
     await new Promise(function (resolve, reject) {
       request(charURL, function (err, res, body) {
         if (err) return console.error(err);
 
-        // Extract the character details, display the character's name, and fulfill the promise to mark the task as complete.
+        // Parse the charcter nformation and print the character's name Resolve the promise to indicate completion
         console.log(JSON.parse(body).name);
         resolve();
       });
     });
   }
-};
+});
